f39a6bc8f4843598665dfa1510ad552e84103d86
deepanshu-chauhan07 <163377322+deepanshu-chauhan07@users.noreply.github.com>
2025-08-31 07:59:16 +0000

    config done

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,23 @@
+let score="33"
+console.log(typeof score)
+console.log(typeof (score))
+//type conversiom
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+let score1=null
+console.log(typeof score)
+//type conversion into number
+//"33"=33
+//"333zbs"=NaN
+//true=1,false=0
+//""=false,"deepu"=true in boolean
+
+//strinh
+let oneNumber=37
+let stringNumber=String(oneNumber)
+  
+console.log(stringNumber)
+console.log(typeof (stringNumber))
+//output
+//37
+//string
