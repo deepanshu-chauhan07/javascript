9c6185646a047074269e0fcea6b175c1ad650e89
deepanshu-chauhan07 <163377322+deepanshu-chauhan07@users.noreply.github.com>
2025-08-31 08:43:16 +0000

    comfig done

---
+++ basics/comparison.js
@@ -0,0 +1,13 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<=1)
+console.log(2==1)
+console.log(2!=1)//true
+console.log("2">1)//true
+console.log("0002">1)//true
+console.log(null<=0)//true
+console.log(null>0)//false
+console.log(null==0)//falsse
+console.log(null>=0)//true
+//strictly=== check krte h value or datatype
+console.log ("2"===2)//always use these comparsision
