bd4c85aa92d6a1b205995d7ac12f4535a4567e2a
deepanshu-chauhan07 <163377322+deepanshu-chauhan07@users.noreply.github.com>
2025-08-31 08:33:50 +0000

    config done

--- basics/datatypeconversion.js
+++ basics/datatypeconversion.js
@@ -21,3 +21,29 @@ console.log(typeof (stringNumber))
 //output
 //37
 //string
+             //OPERATIONS
+let value=3
+let negValue=-value
+console.log(negValue)
+console.log(2-2)
+console.log(2+2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%3)
+
+let str1="deepu"
+let str2=" chauhan "
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)//str+num=12(concat)
+console.log(1+"2")//num+str=12(concat)
+console.log(1+"2"+3)//num+str+num=123(concat)
+console.log(1+2+'3')//num+num=add3
+console.log((1+2)*5%3)
+console.log(+true)
+let gameCounter=100
+gameCounter++ 
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
